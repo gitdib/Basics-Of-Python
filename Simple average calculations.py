39e8a6a5421d54bc65694a7e6f6bfa0dbe06fc1b
Dibyajyoti Roychoudhury <dibyajyotiroychoudhurydj@gmail.com>
2026-09-07 01:28:24 +0530

    Add simple average calculations script

---
+++ Simple average calculations.py
@@ -0,0 +1,8 @@
+a = float(input( "enter first item: "))
+b = float(input( "enter second item: "))
+c = float(input( "enter third item: "))
+d = float(input( "enter fourth item: "))
+e = float(input( "enter fifth item: "))
+
+print(" The total bill amount is: ", a + b + c)
+print(" The average bill amount is: ", (a + b + c) / 3)  # Basics avg calculations can be done using this 
